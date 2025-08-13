f0609b6f50ccb9d3576cd32d07023edda61f98cd
Davi Freitas <davifreitaz999@gmail.com>
2025-08-13 20:05:27 -0300

    adicionando login e registro

--- src/component/sideBar.jsx
+++ src/component/sideBar.jsx
@@ -1,14 +1,16 @@
+import { useNavigate } from "react-router";
 
 
 export default function Sidebar() {
+    const navigate = useNavigate()
   return (
     <div className="fixed top-0 left-0 h-screen w-64 bg-gray-800 text-white flex flex-col">
       <h1 className="text-2xl font-bold p-4 border-b border-gray-700">Meu App</h1>
       <nav className="flex flex-col p-4 space-y-2">
-        <a href="#" className="hover:bg-gray-700 rounded p-2">Dashboard</a>
-        <a href="#" className="hover:bg-gray-700 rounded p-2">Links</a>
+        <a onClick={()=>navigate('/dashboard')} className="hover:bg-gray-700 rounded p-2">Dashboard</a>
+        <a onClick={()=>navigate('/links')} className="hover:bg-gray-700 rounded p-2">meus Links</a>
         <a href="#" className="hover:bg-gray-700 rounded p-2">Configurações</a>
-        <a href="#" className="hover:bg-gray-700 rounded p-2">Logout</a>
+        <a href="../pages/userAutenticado/link" className="hover:bg-gray-700 rounded p-2">Logout</a>
       </nav>
     </div>
   );
