1416b08be331e452446e8f72e65a9888fe02b4d6
Davi Freitas <davifreitaz999@gmail.com>
2025-08-13 19:34:23 -0300

    adicinando rotas e navegacao

---
+++ src/pages/register.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+export default function Register() {
+  return (
+    <div className="flex">
+      
+      <div className="ml-64 p-8 flex-1">
+        <h2 className="text-3xl font-bold mb-4">Conteúdo Principal</h2>
+        <p>Aqui vai o conteúdo da página...</p>
+      </div>
+    </div>
+  );
+}
