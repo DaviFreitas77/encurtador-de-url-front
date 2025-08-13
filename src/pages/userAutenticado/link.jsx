7941ec2828841f474dc7e120c196dbaae0b64423
Davi Freitas <davifreitaz999@gmail.com>
2025-08-13 20:12:19 -0300

    trazendo o array de links no log inicialmente

--- src/pages/userAutenticado/link.jsx
+++ src/pages/userAutenticado/link.jsx
@@ -1,8 +1,33 @@
 import Sidebar from "../../component/sideBar"
-export default function Links(){
-    return(
+import url from '../../../url.json'
+import { useEffect } from "react"
+
+export default function Links() {
+    const token = localStorage.getItem("token")
+
+    useEffect(() => {
+        const myLinks = async () => {
+            try {
+                const response = await fetch(`${url.url}/api/links`, {
+                    method: "GET",
+                    headers: {
+                        'Content-Type': "appliation/json",
+                        'Accept': "application/json",
+                        "Authorization": `Bearer ${token}`
+                    },
+                })
+                const data = await response.json()
+                console.log(data)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        myLinks()
+    }, [])
+
+    return (
         <div>
-            <Sidebar/>
+            <Sidebar />
 
         </div>
     )
