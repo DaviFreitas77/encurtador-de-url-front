fefca7d31b7b6f94ece3ab368b193aa0558937b1
Davi Freitas <davifreitaz999@gmail.com>
2025-08-14 15:04:41 -0300

    fix:arrumando responsivo da dashboard

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -6,6 +6,10 @@ import { BarChart, Bar, XAxis, YAxis, CartesianGrid } from 'recharts';
 import url from '../../url.json'
 export default function Dashboard() {
   const [data, setData] = useState([])
+  const [totClick, setTotClick] = useState([])
+  const [linkActive, setLinkActive] = useState([])
+  const [linkExpired, setLinkExpired] = useState([])
+  const [topClick,setTopClick] = useState([])
   const navigate = useNavigate()
   const token = localStorage.getItem("token")
   useEffect(() => {
@@ -18,7 +22,7 @@ export default function Dashboard() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(`${url.url}/api/countLink`, {
+        const response = await fetch(`${url.url}/api/metrics/total`, {
           method: "GET",
           headers: {
             "Authorization": `Bearer ${token}`
@@ -26,60 +30,136 @@ export default function Dashboard() {
         })
 
         const data = await response.json();
-        console.log(data)
         setData(data)
       } catch (error) {
         console.log(error)
       }
 
     }
+    const fetchTotalClick = async () => {
+      try {
+        const response = await fetch(`${url.url}/api/metrics/total-click`, {
+          method: "GET",
+          headers: {
+            "Authorization": `Bearer ${token}`
+          },
+        })
+
+        const data = await response.json();
+        setTotClick(data)
+      } catch (error) {
+        console.log(error)
+      }
+
+    }
+    const fetchLinkActive = async () => {
+      try {
+        const response = await fetch(`${url.url}/api/metrics/active-expired`, {
+          method: "GET",
+          headers: {
+            "Authorization": `Bearer ${token}`
+          },
+        })
+
+        const data = await response.json();
+    
+        setLinkActive(data.links_active)
+        setLinkExpired(data.links_expired)
+      } catch (error) {
+        console.log(error)
+      }
+
+    }
+    const fetchTopClick = async () => {
+      try {
+        const response = await fetch(`${url.url}/api/metrics/top`, {
+          method: "GET",
+          headers: {
+            "Authorization": `Bearer ${token}`
+          },
+        })
+
+        const data = await response.json();
+
+        setTopClick(data)
+       
+      } catch (error) {
+        console.log(error)
+      }
+
+    }
     fetchData()
+    fetchTotalClick()
+    fetchLinkActive()
+    fetchTopClick()
   }, [])
 
 
   const acitveExpired = [
-    { name: 'Ativos', uv: data.links_active || 0 },
-    { name: 'Expirados', uv: data.links_expired || 0 },
+    { name: 'Ativos', uv: linkActive || 0 },
+    { name: 'Expirados', uv: linkExpired || 0 },
   ]
-  const totalClicks = [
-    { name: 'total', uv: Number(data.total_click) || 0 },
-    
-  ]
-const dataf = data.top_click?.map(link => ({
-  name: link.original_url,
-  click: Number(link.click_count) || 0,
+  const totalLink = [
+    { name: 'total', uv: data.total_links || 0 },
 
-})) || [];
+  ]
+
+  const totalClick = [
+    { name: 'total', uv: Number(totClick.total_click) || 0 },
+
+  ]
+
+  const dataf = topClick.top_click?.map(top => ({
+    name: top.original_url,
+    click: top.click_count || 0,
+  })) || [];
+
   
+
   return (
     <div className="flex">
       <Sidebar />
       <div className="ml-64 p-8 flex-1">
         <h2 className="text-3xl font-bold mb-4">DASHBOARD</h2>
 
-        <div className="flex mt-16">
+        <div className="flex flex-wrap mt-16 gap-8 justify-center">
+
           <div className="flex flex-col justify-center items-center">
-            <h1 className="text-3xl">links ativos e expirados</h1>
+            <h1 className="text-3xl">total de click</h1>
             <PieChart width={350} height={350}>
               <Pie
                 activeShape={{
                   fill: 'red',
                 }}
-                data={acitveExpired}
+                data={totalClick}
                 dataKey="uv"
               />
               <Tooltip defaultIndex={2} />
             </PieChart>
           </div>
           <div className="flex flex-col justify-center items-center">
-            <h1 className="text-3xl">Total de clicks</h1>
+            <h1 className="text-3xl">Total de links</h1>
             <PieChart width={350} height={350}>
               <Pie
                 activeShape={{
                   fill: 'purple',
                 }}
 
-                data={totalClicks}
+                data={totalLink}
+                dataKey="uv"
+              />
+              <Tooltip defaultIndex={2} />
+            </PieChart>
+          </div>
+          <div className="flex flex-col justify-center items-center">
+            <h1 className="text-3xl">ativos e expirados</h1>
+            <PieChart width={350} height={350}>
+              <Pie
+                activeShape={{
+                  fill: 'purple',
+                }}
+
+                data={acitveExpired}
                 dataKey="uv"
               />
               <Tooltip defaultIndex={2} />
