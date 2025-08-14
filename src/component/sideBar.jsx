a6d5ec1dda6994aa7f647e2c5f3a2050c423e7ef
Davi Freitas <davifreitaz999@gmail.com>
2025-08-14 14:13:52 -0300

    fix:arrumando rota do dashboard

--- src/component/sideBar.jsx
+++ src/component/sideBar.jsx
@@ -4,7 +4,6 @@ import url from '../../url.json'
 export default function Sidebar() {
   const token = localStorage.getItem("token")
  const navigate = useNavigate()
-
   const logout = async() =>{
     const response = await fetch(`${url.url}/api/auth/logout`,{
       method:"POST",
@@ -28,11 +27,11 @@ export default function Sidebar() {
     <div className="fixed top-0 left-0 h-screen w-64 bg-gray-800 text-white flex flex-col">
       <h1 className="text-2xl font-bold p-4 border-b border-gray-700">Meu App</h1>
       <nav className="flex flex-col p-4 space-y-2">
-        <a onClick={() => navigate('/dashboard')} className="hover:bg-gray-700 rounded p-2">Dashboard</a>
+        <a onClick={() => navigate('/')} className="hover:bg-gray-700 rounded p-2">Dashboard</a>
         <a onClick={() => navigate('/criar-link')} className="hover:bg-gray-700 rounded p-2">Criar link</a>
         <a onClick={() => navigate('/links')} className="hover:bg-gray-700 rounded p-2">meus Links</a>
         <a href="#" className="hover:bg-gray-700 rounded p-2">Configurações</a>
-        <a onClick={logout} className="hover:bg-gray-700 rounded p-2">Logout</a>
+        <a onClick={logout} className="hover:bg-gray-700 rounded p-2 cursor-pointer">Logout</a>
       </nav>
     </div>
   );
