e098999fb18a1322657b77696091a3302f53c0ae
Davi Freitas <davifreitaz999@gmail.com>
2025-08-14 01:53:15 -0300

    fix:arrumando layout

--- src/pages/userAutenticado/link.jsx
+++ src/pages/userAutenticado/link.jsx
@@ -64,50 +64,50 @@ export default function Links() {
     return (
         <div >
             <Sidebar />
-            <div className="flex items-center justify-center flex-col">
+            <div className="flex items-center 2xl:items-end justify-center flex-col">
                 {qlUrl && (
                     <img src={qlUrl} alt="" />
                 )}
-                {links && links.map((link) => (
-                    <div
-                        key={link.id}
-                        className="border p-3 rounded bg-white shadow-sm flex flex-col mb-4"
-                    >
-                        <button
-                            onClick={() => window.open(`${url.url}/api/s/${link.slug}`, "_blank")}
-                            className="text-blue-500 font-medium break-all max:w-[500px] cursor-pointer"
+                <div className="grid  2xl:grid-cols-3  xl:grid-cols-2 gap-2  ml-36">
+                    {links && links.map((link) => (
+                        <div
+                            key={link.id}
+                            className="border p-3 rounded bg-white shadow-sm flex flex-col mb-4"
                         >
-                            {`${url.url}/s/${link.slug}`}
-                        </button>
-                        <button
-                            onClick={() => generateQRCode(link.slug)}
-                            className="text-blue-500 font-medium break-all max:w-[500px] cursor-pointer"
-                        >
-                            gerar qrcode
-                        </button>
-
-                        <span className="text-gray-600 text-sm">
-                            Status: <b>{link.status}</b>
-                        </span>
-                        <span className="text-gray-600 text-sm">
-                            {link.expires_at && (
-                                link.status === 'expired' ? (
-                                    <b>expirou em: {link.expires_at}</b>
-                                ) : (
-                                    <b>expira em: {link.expires_at}</b>
-                                )
-                            )}
-                        </span>
-
-                        <a
-                            href={link.original_url}
-                            target="_blank"
-                            className="text-blue-500 font-bold text-base truncate max-w-[200px] block"
-                        >
-                            {link.original_url}
-                        </a>
-                    </div>
-                ))}
+                            <button
+                                onClick={() => window.open(`${url.url}/api/s/${link.slug}`, "_blank")}
+                                className="text-blue-500 font-medium break-all max:w-[500px] cursor-pointer"
+                            >
+                                {`${url.url}/s/${link.slug}`}
+                            </button>
+                            <button
+                                onClick={() => generateQRCode(link.slug)}
+                                className="text-blue-500 font-medium break-all max:w-[500px] cursor-pointer"
+                            >
+                                gerar qrcode
+                            </button>
+                            <span className="text-gray-600 text-sm">
+                                Status: <b>{link.status}</b>
+                            </span>
+                            <span className="text-gray-600 text-sm">
+                                {link.expires_at && (
+                                    link.status === 'expired' ? (
+                                        <b>expirou em: {link.expires_at}</b>
+                                    ) : (
+                                        <b>expira em: {link.expires_at}</b>
+                                    )
+                                )}
+                            </span>
+                            <a
+                                href={link.original_url}
+                                target="_blank"
+                                className="text-blue-500 font-bold text-base truncate max-w-[200px] block"
+                            >
+                                {link.original_url}
+                            </a>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     )
